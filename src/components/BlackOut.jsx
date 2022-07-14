1fa61a5adcfb76497775a19581f7930d52340940
manugarderes <69734460+manugarderes@users.noreply.github.com>
2022-07-14 12:00:46 -0300

    Tildes

--- src/components/BlackOut.jsx
+++ src/components/BlackOut.jsx
@@ -7,21 +7,21 @@ function BlackOut() {
       {show && (
         <div className="BlackOut">
           <h1 style={{ marginTop: "10px", textAlign: "center" }}>
-            QuaLabs - Prueba Tecnica - Manuel Garderes
+            QuaLabs - Prueba Técnica - Manuel Garderes
           </h1>
           <h3 style={{ marginTop: "10px", textAlign: "center" }}>
             Front End - React App
           </h3>
           <p style={{ marginTop: "10px", textAlign: "center" }}>
-            Dentro de la aplicaión puedes navegar por los diferentes modulos y
-            se mostraran la lista de usuarios que lo usan.
+            Dentro de la aplicación puedes navegar por los diferentes módulos y
+            se mostrarán la lista de usuarios que lo usan.
           </p>
           <p style={{ marginTop: "10px", textAlign: "center" }}>
-            El diseño esta basado 100% en el mock entregado.
+            El diseño está basado 100% en el mock entregado.
           </p>
           <p style={{ marginTop: "10px", textAlign: "center" }}>
             Los 4 botones de la parte inferior son solo visuales, no entran
-            dentro de las funcionalidades de la aplicaión.
+            dentro de las funcionalidades de la aplicación.
           </p>
           <p style={{ marginTop: "10px", textAlign: "center" }}>
             <a target={"_blank"} style={{color:"white"}} href="https://github.com/manugarderes/Qualabs-Prueba-Tecnica">Repositorio en GitHub</a>
